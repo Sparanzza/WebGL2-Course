5d3dc9238b8ab67fb57a5b14fa2a87c5e4b5e614
Sparanzza <aurelio@sparanzza.com>
2019-05-17 23:57:30 +0100

    triangle vertexcolor webgl

--- js/main.js
+++ js/main.js
@@ -18,15 +18,25 @@ function start(){
     gl.bufferData(gl.ARRAY_BUFFER , new Float32Array( triangleVertices ) 
 , gl.STATIC_DRAW);
 
-    var triangleColors = [
-        1.0 , 0.0 , 0.0 , 1.0,
-        0.0 , 1.0 , 0.0 , 1.0,
-        0.0 , 0.0 , 1.0 , 1.0
-    ];
+var triangleColors = [
+    1.0 , 0.0 , 0.0 , 1.0,
+    0.0 , 1.0 , 0.0 , 1.0,
+    0.0 , 0.0 , 1.0 , 1.0
+];
 
-    var triangleVertexColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER , triangleVertexColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER , new Float32Array(triangleColors), gl.STATIC_DRAW)
+var triangleVertexColorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER , triangleVertexColorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER , new Float32Array(triangleColors), gl.STATIC_DRAW);
+
+var trianglePositionAndColors = [
+    1.0 ,-1.0 , 0.0 ,1.0 , 0.0 , 0.0 , 1.0,
+    0.0 , 1.0 , 0.0 , 0.0 , 1.0 , 0.0 , 1.0,
+    -1.0, -1.0 , 0.0, 0.0 , 0.0 , 1.0 , 1.0
+];
+
+var triangleVertexPositionAndColorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER , triangleVertexPositionAndColorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER , new Float32Array(trianglePositionAndColors), gl.STATIC_DRAW)
 
     var vertexShader = getAndCompileShader("vertexShader");
     var fragmentShader = getAndCompileShader("fragmentShader");
@@ -40,6 +50,33 @@ function start(){
     }
 
     gl.useProgram(shaderProgram);
+
+    const FLOAT_SIZE  = 4;
+
+    var positionAttributeLocation = gl.getAttribLocation(shaderProgram, "position");
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    // gl.bindBuffer(gl.ARRAY_BUFFER , triangleVertexPositionBuffer);
+    // gl.vertexAttribPointer(positionAttributeLocation , 3 , gl.FLOAT, 0,0);
+
+    var colorAttributeLocation = gl.getAttribLocation(shaderProgram, "color");
+    gl.enableVertexAttribArray(colorAttributeLocation);
+    // gl.bindBuffer(gl.ARRAY_BUFFER , triangleVertexColorBuffer);
+    // gl.vertexAttribPointer(colorAttributeLocation , 3 , gl.FLOAT, 0,0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER , triangleVertexPositionAndColorBuffer);
+    gl.vertexAttribPointer(positionAttributeLocation , 3 , gl.FLOAT,false, 7*FLOAT_SIZE,0);
+    gl.vertexAttribPointer(colorAttributeLocation , 4 , gl.FLOAT,false, 7*FLOAT_SIZE,3*FLOAT_SIZE);
+
+    requestAnimationFrame(RunRenderLoop);
+
+    function RunRenderLoop(){
+
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.clearColor(0,0,0,1);
+        gl.drawArrays(gl.TRIANGLES , 0 , 3);
+        requestAnimationFrame(RunRenderLoop);
+    }
+
 }
 
 function getAndCompileShader (id){
